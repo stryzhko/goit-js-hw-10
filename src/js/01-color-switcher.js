c5c8b389fa50a31f557283544d3d491f6ea10350
stryzhko <stryzhko@gmail.com>
2023-01-27 23:10:13 +0200

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
 
+refs.startBtn.addEventListener('click', startChangeColor);
+refs.stopBtn.addEventListener('click', stopChangeColor);
+refs.stopBtn.disabled = true;
+
+let timerId = null;
+
+function startChangeColor() {
+  timerId = setInterval(() => {
+      refs.startBtn.disabled = true;
+      refs.stopBtn.disabled = false;
+    document.body.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+}
+
+function stopChangeColor() {
+  clearInterval(timerId);
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
